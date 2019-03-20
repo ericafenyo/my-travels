24c5053afa0971a02bd7a32960c660cc1f0c79a2
ericafenyo <ericafenyo@yahoo.com>
2019-03-20 13:23:29 +0100

    add files, implement card views using bootstarp

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Travel from './Travel';
+
+
+const data = [
+  {
+    destination: "Paris",
+    country: "France",
+
+    photo: "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1607&h=1607&q=80",
+    distance: "1 987 km"
+  },
+
+  {
+    destination: "Moscou",
+    country: "Russia",
+    photo: "https://images.unsplash.com/photo-1504615458222-979e04d69a27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1607&h=1607&q=80",
+    distance: "5 836 km"
+  },
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div className="App container">
+        <div className=" travel d-flex flex-wrap justify-content-around mt-4">
+          {data.map((value, index) => <Travel data={value} key={index} />)}
+        </div>
+      </div>);
   }
 }
 
---
+++ src/Travel.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Travel extends Component {
+  render() {
+    const data = this.props.data
+    return (
+      <div>
+        <div className="card mb-3" style={{ width: "400px" }}>
+          <img src={data.photo} className="card-img-top" alt="Country Thumbnail"></img>
+          <div className="card-body p-0">
+            <ul className="list-unstyled text-left p-2 m-0" style={{ fontSize: "16px" }}>
+              <li>Destination: {data.destination}</li>
+              <li>Country: {data.country}</li>
+              <li>Distance: {data.distance}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Travel
